77facb49be10e6911a6c277726ac2d0f306155c2
Dohun Kim <yh08037@gmail.com>
2020-03-04 15:57:33 +0900

    created toggle switch

--- App.js
+++ App.js
@@ -1,29 +1,53 @@
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  StatusBar
-} from "react-native";
+import React, { useState, Component } from "react";
+import { StyleSheet, Text, View, StatusBar, Switch } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.halfContainer}>
-        <Text style={styles.textTitle}>KNU Notice</Text>
+export default class App extends Component {
+  state = {
+    isNoticeOn: true,
+    isCoronaOn: true
+  };
+
+  _toggleNotice = () =>
+    this.setState(state => ({
+      isNoticeOn: !state.isNoticeOn
+    }));
+
+  _toggleCorona = () =>
+    this.setState(state => ({
+      isCoronaOn: !state.isCoronaOn
+    }));
+
+  render() {
+    console.log(this.state.switchValue);
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.halfContainer}>
+          <Text style={styles.textTitle}>KNU Notice</Text>
+        </View>
+        <View style={styles.halfContainer}>
+          <View style={styles.elem}>
+            <Text style={styles.textSwitch}>경북대학교 공지사항</Text>
+            <Switch
+              style={styles.switch}
+              onValueChange={this._toggleNotice}
+              value={this.state.switchValue}
+              trackColor="orange"
+            />
+          </View>
+          <View style={styles.elem}>
+            <Text style={styles.textSwitch}>코로나19 대응 관련 공지</Text>
+            <Switch
+              style={styles.switch}
+              onValueChange={this._toggleCorona}
+              value={this.state.switchValue}
+              trackColor="orange"
+            />
+          </View>
+        </View>
       </View>
-      <View style={styles.halfContainer}>
-        <TouchableOpacity
-          onPress={() => alert("Hello, world!")}
-          style={styles.button}
-        >
-          <Text style={styles.textButton}>button</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -42,12 +66,23 @@ const styles = StyleSheet.create({
   textTitle: {
     fontSize: 60
   },
-  textButton: {
-    fontSize: 40
+
+  elem: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    borderColor: "#eee",
+    borderBottomWidth: 0.5,
+    paddingLeft: 5
   },
-  button: {
-    backgroundColor: "skyblue",
-    padding: 20,
-    borderRadius: 5
+  textSwitch: {
+    fontSize: 16,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  switch: {
+    padding: 8,
+    paddingLeft: 5
   }
 });
